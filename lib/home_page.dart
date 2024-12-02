830e2e70184bb1b08b4b5345b4c979f230be98cd
muameh <balmehmet111@gmail.com>
2024-12-02 11:52:13 +0300

    first commit for flutter <3

---
+++ lib/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Text(
+                  "Text",
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 60,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(color: Colors.black, offset: Offset(2, 2), blurRadius: 4.0)
+                      ]),
+                )),
+            const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Text("Text"),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(onPressed: () {}, child: Text("Elevated button"))),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: "Enter your mail address here", // Kullanıcıya ipucu metni
+                  labelText: "mail adress", // Alanın başlığı
+                  border: OutlineInputBorder(), // Çerçeve
+                ),
+                onChanged: (value) {
+                  print("Input: $value"); // Kullanıcı yazdıkça çalışır
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: TextButton(onPressed: () {}, child: const Text("TextButton")),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: OutlinedButton(onPressed: () {}, child: const Text("OutlinedButton")),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add, color: Colors.black),
+      ),
+    );
+  }
+}
